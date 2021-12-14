6a620a3c5861b7cf5f05da648826a696af4e5729
AkopGasparyan <akop989@gmail.com>
2021-12-14 01:37:57 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -29,7 +29,7 @@ function age() {
 
     if (y > 4 && y <12) {
         clearly = "VAX";
-    } else if (y > 100 && y <0) {
+    } else if (y > 100 OR y <0) {
     
         clearly = "Enter correct DOB".fontcolor("red");
     
